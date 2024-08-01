3e55662c0265150a9136ad51e79924444f064be0
Ayush Mishhra <aayushmishra7398@gmail.com>
2024-08-01 10:57:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" //Treat all JS code as newer version
+
+// alert(3 + 3) //We are using nodejs not browser
+
+console.log(3 
+    +
+     3); //Code readability should be high.
+
+     console.log("Ayush");
+
+     let name = "Ayush"
+     let age = 19
+     let isLoggedIn = true
+     let state = null
+
+     // number => 2 to power 53
+     // bigint
+     // string => ""
+     // boolean = true/false
+     // null => standalone value
+     // undefined => value is not assigned yet
+     // symbol => use to find the uniqueness between components in react (in most if the cases)
+
+     // object
+
+     console.log(typeof "Ayush");
+     console.log(typeof age);
+
+     console.log(typeof null);
+     console.log(typeof undefined);
