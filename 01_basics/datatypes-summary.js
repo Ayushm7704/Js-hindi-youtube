77cc7277d60fd073747edbd841495965d2e6f5f8
Ayush Mishhra <aayushmishra7398@gmail.com>
2024-08-05 07:39:50 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// Primitive
+
+// 7 Types: String, Number, Boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+const outsideTemp = null
+let userEmai;;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
