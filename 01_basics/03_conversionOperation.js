31d83c29f99793104c6ee3dd59a6cba711564c6f
Ayush Mishhra <aayushmishra7398@gmail.com>
2024-08-05 06:18:22 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+// let score = "33abc"
+
+// let score = null
+
+// let score = undefined
+
+// let score = true
+
+// let score = "Ayush"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(score);
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+
+Notes- 
+       (01- "33" => 33
+       (02- "33abc" => NaN
+       (03- true => 1; false => 0
+
+*/
+
+// let isLoggedIn = 1
+
+// let isLoggedIn = ""
+
+// let isLoggedIn = "Ayush"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+
+Notes-
+       (01- 1 => true; 0 => false
+       (02- "" => False
+       (03- "Ayush" => ture
+*/
+
+let num1 = 33
+
+let stringNum1 = String(num1)
+console.log(stringNum1);
+console.log(typeof stringNum1);
+
+
