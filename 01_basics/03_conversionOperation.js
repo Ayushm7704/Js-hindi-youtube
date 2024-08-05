54eae59dad6503358a943db0a6c529576a5e1864
Ayush Mishhra <aayushmishra7398@gmail.com>
2024-08-05 06:57:38 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,10 +43,42 @@ Notes-
        (03- "Ayush" => ture
 */
 
-let num1 = 33
+// let num1 = 33
 
-let stringNum1 = String(num1)
-console.log(stringNum1);
-console.log(typeof stringNum1);
+// let stringNum1 = String(num1)
+// console.log(stringNum1);
+// console.log(typeof stringNum1);
 
 
+// ***************************** Operations ******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Ayush"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+console.log(+"");
+
+
+let gameCounter = 100
+gameCounter++
+++gameCounter
+console.log(gameCounter);
