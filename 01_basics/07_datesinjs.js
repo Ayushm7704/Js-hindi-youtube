0bf5f9d726be9d2beb3bca5f66dbffc5525aaccf
Ayush Mishhra <aayushmishra7398@gmail.com>
2024-08-06 05:50:18 +0000

    dates in js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(typeof Date);
+
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 4)
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
